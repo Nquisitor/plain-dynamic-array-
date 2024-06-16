701dde7146a5ac8afd840c192a338c872abbab99
Nquisitor <154739902+Nquisitor@users.noreply.github.com>
2024-06-16 15:21:10 +0300

    Add files via upload

---
+++ ConsoleApplication4.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+class Dynamic_array
+{
+private:
+	int arr[100]{};
+	int* ptr = &arr[0];
+	size_t size = 0;
+
+public:
+	Dynamic_array()
+	{}
+
+	explicit Dynamic_array(int n) : size(n)
+	{}
+
+	void push_back(int& elem)
+	{
+		*ptr = elem;
+		ptr++;
+		size++;
+	}
+
+	bool empty() const
+	{
+		return arr[0] == 0;
+	}
+
+	void clear()
+	{
+		while (ptr != &arr[0])
+		{
+			*ptr = 0;
+			ptr--;
+		}
+		*ptr = 0;
+		size = 0;
+	}
+
+	size_t volume() const
+	{
+		return size;
+	}
+
+	void exterminate()
+	{
+		ptr--;
+		*ptr = 0;
+		size--;
+	}
+};
+
+int main(int argc, const char* argv[])
+{
+	int n;
+	std::cin >> n;
+	Dynamic_array arr;
+	while (n--)
+	{
+		int elem;
+		std::cin >> elem;
+		if (elem >= 0) arr.push_back(elem);
+	}
+	int size = arr.volume();
+	std::cout << size << '\t';
+	arr.exterminate();
+	std::cout << arr.volume() << '\t';
+	arr.clear();
+	std::cout << arr.volume();
+
+	return 0;
+}
